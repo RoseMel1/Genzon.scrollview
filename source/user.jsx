355889c04d2a8f1ef6c9c7e84b4521606807807b
JunreyG <143053124+RoseMel1@users.noreply.github.com>
2023-11-27 17:48:44 +0800

    Initial change

---
+++ source/user.jsx
@@ -0,0 +1,83 @@
+export const users = [
+    {
+      id: 1,
+      name: {
+        firstname: " NIKKO BRYLLE",
+        middlename: "",
+        lastname: "ALTURAS",
+      },
+      address: "Los Angeles, California",
+    },
+    {
+      id: 2,
+      name: {
+        firstname: " CHARIZ MAE",
+        middlename: "T",
+        lastname: "BACO",
+      },
+      address: "Los Angeles, California",
+    },
+    {
+      id: 3,
+      name: {
+        firstname: " ANALIE",
+        middlename: "",
+        lastname: "BARBANIDA",
+      },
+      address: "Las Vegas, Nevada",
+    },
+    {
+      id: 4,
+      name: {
+        firstname: " RIZA",
+        middlename: "",
+        lastname: "BATULANON",
+      },
+      address: "Pari, France",
+    },
+    {
+      id: 5,
+      name: {
+        firstname: " NIKKO BRYLLE",
+        middlename: "",
+        lastname: "ALTURAS",
+      },
+      address: "Los Angeles, California",
+    },
+    {
+      id: 6,
+      name: {
+        firstname: " CHARIZ MAE",
+        middlename: "T",
+        lastname: "BACO",
+      },
+      address: "Los Angeles, California",
+    },
+    {
+      id: 7,
+      name: {
+        firstname: " ANALIE",
+        middlename: "",
+        lastname: "BARBANIDA",
+      },
+      address: "Las Vegas, Nevada",
+    },
+    {
+      id: 8,
+      name: {
+        firstname: " RIZA",
+        middlename: "",
+        lastname: "BATULANON",
+      },
+      address: "Paris, France",
+    },
+    {
+      id: 9,
+      name: {
+        firstname: " NIKKO BRYLLE",
+        middlename: "",
+        lastname: "ALTURAS",
+      },
+      address: "Los Angeles, California",
+    },
+  ];
